302399e6acb455510a83371028a05fc3092cb2dc
Akassharjun Shanmugarajah <akassharjun@gmail.com>
2019-09-07 14:02:01 +0530

    Completed sample project.

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # sample-golang-rest-api-with-mongo-db
+
+Basic RESTful API to save the first name & last name of a person. 
+
+### Requirements
+
+* Local instance of MongoDB
+* Go
+
+### API Endpoints
+
+#### getPerson
+
+```
+URL
+http://localhost:9323/person/{id}
+```
+
+
+#### createPerson
+
+```
+URL
+http://localhost:9323/person/
+
+Body
+{ 
+  "firstname" : "FIRST NAME",
+  "lastname" : "LAST NAME" 
+}
+```
+
+#### get People
+
+```
+URL
+http://localhost:9323/people/
+```
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
+
+type Person struct {
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	FirstName string             `json:"firstname,omitempty" bson:"firstname,omitempty"`
+	LastName  string             `json:"lastname,omitempty" bson:"lastname,omitempty"`
+}
+
+func CreatePersonEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var person Person
+	_ = json.NewDecoder(request.Body).Decode(&person)
+	collection := client.Database("sample-golang-rest-api-with-mongo-db").Collection("people")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	result, _ := collection.InsertOne(ctx, person)
+	json.NewEncoder(response).Encode(result)
+}
+
+func GetPeopleEndpoint(response http.ResponseWriter, request *http.Request) {}
+
+func GetPersonEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	params := mux.Vars(request)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	var person Person
+	collection := client.Database("sample-golang-rest-api-with-mongo-db").Collection("people")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	err := collection.FindOne(ctx, Person{ID: id}).Decode(&person)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	_ = json.NewEncoder(response).Encode(person)
+}
+
+func DefaultEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Write([]byte(`{ "message": "` + "Welcome to the sample-golang-rest-api-with-mongodb" + `" }`))
+}
+
+func main() {
+	fmt.Println("Starting the application...")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, _ = mongo.Connect(ctx, clientOptions)
+	router := mux.NewRouter()
+	router.HandleFunc("/", DefaultEndpoint).Methods("GET")
+	router.HandleFunc("/person", CreatePersonEndpoint).Methods("POST")
+	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
+	router.HandleFunc("/person/{id}", GetPersonEndpoint).Methods("GET")
+	http.ListenAndServe(":9323", router)
+}
